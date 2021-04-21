ecc473fb8b349ba64c79c4cd34d2ed73717f97eb
OT <o.trotman5@gmail.com>
2021-04-21 16:22:55 -0400

    Add project files.

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FirstWebAPIDemo.Models;
+
+namespace FirstWebAPIDemo.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBContext dbContext = new EmployeeDBContext())
+            {
+                return dbContext.Employees.ToList();
+            }
+        }
+        public Employee Get(int id)
+        {
+            using (EmployeeDBContext dbContext = new EmployeeDBContext())
+            {
+                return dbContext.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FirstWebAPIDemo.Controllers
+{
+    //[Authorize]
+    public class ValuesController : ApiController
+    {
+        static List<string> strings = new List<string>()
+        {
+            "value0", "value1", "value2"
+        };
+
+        /// <summary>
+        /// Get All the Values
+        /// </summary>
+        /// <remarks>
+        /// Get All the String Values
+        /// </remarks>
+        /// <returns></returns>
+
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            //return new string[] { "value1", "value2" };
+            return strings;
+        }
+
+        // GET api/values/5
+        /*public string Get(int id)
+        {
+            return "value";
+        }*/
+        public string Get(int id)
+        {
+            return strings[id];
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+            strings.Add(value);
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+            strings[id] = value;
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            strings.RemoveAt(id);
+        }
+    }
+}
